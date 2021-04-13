778e36cae92de65f4661b4ccfaffb165ee0b8084
Anne Blankert <anneb@geodan-latitude-e5550.geodan.nl>
2021-04-13 10:42:51 +0200

    security update and add unique id

--- configserver.js
+++ configserver.js
@@ -6,6 +6,8 @@ const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const fs = require('fs');
 const uuidv4 = require('uuid/v4');
+const md5 = require('md5')
+const sanitize = require("sanitize-filename");
 const config = require('./config.json');
 
 const app = express();
@@ -15,16 +17,20 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(fileUpload({limits: { fileSize: 10 * 1024 * 1024 }}));
 app.use('/', express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: true}));
 
 function validateConfig(json) {
     return (json.map && json.datacatalog && json.tools && json.keys)
 }
 
-app.post('/', (req, res, next) => {
+app.post('/', async (req, res, next) => {
     if (Object.keys(req.files).length == 0) {
       return res.status(400).send('No files were uploaded.');
     }
-    
+    let key = req.body.key;
+    let hash = md5(key).slice(0,12)
+    //console.log(`key: ${key}, hash: ${hash}`)
+
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let configFile = req.files.configfile;
     if (configFile.truncated) {
@@ -38,15 +44,24 @@ app.post('/', (req, res, next) => {
     } catch (error) {
         return res.status(422).json({"error": `Error parsing json: ${error}`})
     }
-    
+    let hashedDirname =`${__dirname}/public/files/${hash}`;
+    fs.mkdirSync(hashedDirname, {recursive: true});
+
 
     // Use the mv() method to place the file somewhere on your server
-    const filename = `${uuidv4()}.json`;
-    configFile.mv(`${__dirname}/public/files/${filename}`, function(err) {
+    let storageFilename = req.body.filename;
+    let filename;
+    if (storageFilename && storageFilename !== '') {
+        filename = sanitize(storageFilename);
+    } else {
+        //filename = `${uuidv4()}.json`;
+        filename = sanitize(req.files.configfile.name)
+    }
+    configFile.mv(`${hashedDirname}/${filename}`, function(err) {
     if (err) {
         return res.status(500).json({error: err});
     }
-    const url = `${req.protocol}://${req.hostname}${`${config.pathprefix}${req.originalUrl}/files/${filename}`.replace(/\/\//g, '/')}`;
+    const url = `${req.protocol}://${req.hostname}${`${config.pathprefix}${req.originalUrl}/files/${hash}/${filename}`.replace(/\/\//g, '/')}`;
     res.json({"file": filename, "url": url, "use": config.template.replace('{url}', url)});
     const a = req;
     });
